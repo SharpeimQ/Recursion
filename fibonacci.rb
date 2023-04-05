683fb87e77400833948d8291cc9adbf5fd69ced3
SharpeimQ <SharpHaiyan@gmail.com>
2023-04-05 16:02:06 -0400

    Build fibonacci recursive method

--- fibonacci.rb
+++ fibonacci.rb
@@ -6,12 +6,10 @@
 # fibonacci sums each number with the one before it! starting from 0.
 # the number inputted in the method does not affect the sequence, only the duration of it
 # since the number sets the duration of the sequence, the number sets the amount of iterations
-# for every iteration, add the last element with the second last element and 
+# for every iteration, add the last element with the second last element and
 # push that into an array
-def fib(number)
-  fib_array = [0, 1]
-  return fib_array[0..-2] if number <= 0
-  return fib_array[0..1] if number == 1
+def fib(number, fib_array = [0, 1])
+  return fib_array[0..number] if number <= 1
 
   number.times do
     fib_array << fib_array[-1] + fib_array[-2]
@@ -19,7 +17,17 @@ def fib(number)
   fib_array[0..number - 1]
 end
 
-puts fib(6).inspect
-
+puts fib(7).inspect
 
 # fibonacci recursive
+# base case: There are two conditions to break; if number is 0 and 1
+# recursive case: You can add the last two elements recursively until number is <= 1
+# return an array with the correct number of elements
+def fib_rec(number, fib_array = [0, 1])
+  return fib_array[0..number] if number <= 1
+
+  fib_array << fib_array[-1] + fib_array[-2]
+  fib_rec(number - 1, fib_array)
+end
+
+puts fib(7).inspect
